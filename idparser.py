f7691c23b3616ee53994c6e5728b08ea8348aa74
misson20000 <xenotoad@xenotoad.net>
2018-09-18 15:52:13 -0700

    add 6.0.0 to versioninfo list

--- idparser.py
+++ idparser.py
@@ -53,7 +53,7 @@ decoratorType
 funcDef = doc:{ comment }* decorators:{ decorator }* '[' cmdId:number ']' name:name inputs:namedTuple [ '->' outputs:( namedType | namedTuple ) ] ';' ;
 '''
 
-versionInfo = [ '1.0.0', '2.0.0', '2.1.0', '2.2.0', '2.3.0', '3.0.0', '3.0.1', '3.0.2', '4.0.0', '4.0.1', '4.1.0', '5.0.0', '5.0.1', '5.0.2', '5.1.0']
+versionInfo = [ '1.0.0', '2.0.0', '2.1.0', '2.2.0', '2.3.0', '3.0.0', '3.0.1', '3.0.2', '4.0.0', '4.0.1', '4.1.0', '5.0.0', '5.0.1', '5.0.2', '5.1.0', '6.0.0']
 
 class Semantics(object):
 	def number(self, ast):
